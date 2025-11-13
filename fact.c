96497d4a66412240087b2ea24aa1fdaec2789793
Imanmajee25 <majeeiman25@gmail.com>
2025-11-13 14:20:56 +0530

    Add files via upload

---
+++ fact.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int fact(int number)
+{   
+    int ans;
+    ans=1;
+    for(int i=1;i<=number;i++) ans=ans*i;
+    return ans; 
+}
+int main()
+{
+    int n,r,factn,factr,factnr;
+    int fact (int number);
+    scanf("%d %d",&n,&r);
+    factn=fact(n);
+
+    factr=fact(r);
+    
+    factnr=fact(n-r);
+
+    printf("ncr = %d",factn/(factr*factnr));
+
+    return 0;    
+    
+}
